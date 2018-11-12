d72fe84fb7cfbbabf15a3958d3379727c17a460e
Serhii_Bilorus <serhii_bilorus@epam.com>
2018-11-12 18:08:29 +0200

    first commit c5.1

--- Kotlin-Samples/src/main/kotlin/chapter2/Recipe8.kt
+++ Kotlin-Samples/src/main/kotlin/chapter2/Recipe8.kt
@@ -1,16 +1,18 @@
 package chapter2
 
 import com.google.gson.Gson
+import utils.logger
 
 /**
  * Chapter: Expressive functions and adjustable interfaces
  * Recipe: Smart types checking with generic reified parameters
  */
 fun main(vararg args: String) {
-    val jsonString = ""
+    val jsonString = "{gifsWithPandas:[first.gif, second.gif, third.gif]}"
     val response = Gson().fromJson<ApiResponse>(jsonString)
+    logger<Any>().info(Gson().toJson(response))
 }
-data class ApiResponse(val gifsWithPandas: List<ByteArray>)
+data class ApiResponse(val gifsWithPandas: List<String>)
 
 
 inline fun <reified T> Gson.fromJson(json: String): T {
